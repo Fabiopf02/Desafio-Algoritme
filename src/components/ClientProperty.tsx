1c1af529eef4d183b0fb8aa19bc7f19ae2fcd4f3
Fabiopf02 <fabiofranca593@gmail.com>
2021-10-15 23:00:37 -0300

    remove unused props

--- src/components/ClientProperty.tsx
+++ src/components/ClientProperty.tsx
@@ -3,11 +3,9 @@ import React, { memo } from 'react';
 interface IProps {
   prop: string;
   value: any;
-  btn?: boolean;
-  fct?: () => Promise<void>;
 }
 
-const ClientProperty: React.FC<IProps> = ({ prop, value, btn, fct }) => {
+const ClientProperty: React.FC<IProps> = ({ prop, value }) => {
   return (
     <div className="block">
       <h4 className="p">{prop}</h4>
